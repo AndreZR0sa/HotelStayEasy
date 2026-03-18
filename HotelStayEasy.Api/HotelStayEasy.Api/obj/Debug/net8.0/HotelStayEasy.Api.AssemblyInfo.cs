4cfeb97f1cc9a5b3a30703a19380a1dbf1c15279
AndreZR0sa <andre.z.rosa@aluno.senai.br>
2026-03-18 09:15:54 -0300

    -

--- HotelStayEasy.Api/HotelStayEasy.Api/obj/Debug/net8.0/HotelStayEasy.Api.AssemblyInfo.cs
+++ HotelStayEasy.Api/HotelStayEasy.Api/obj/Debug/net8.0/HotelStayEasy.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelStayEasy.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95c24f9bedc6c643ab45ad6a7aa4fb45b60db043")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31c586bf5941f99a061c77814bbd3a2caa3fc230")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelStayEasy.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelStayEasy.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
